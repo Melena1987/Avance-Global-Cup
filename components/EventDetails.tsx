8d2d2f14a4d9cfa62603d837594f575bb9b6b6b0
Manu <manudesignsforyou@gmail.com>
2025-11-01 09:52:56 +0100

    feat: Update event format description
    Refine the description of the event format to provide more detail about the group stage and placement games.

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -36,7 +36,7 @@ const EventDetails: React.FC = () => {
                 <h2 className="text-4xl font-extrabold text-white text-center mb-12">The Event: Beyond the game</h2>
                 <div ref={gridRef} className="grid md:grid-cols-3 gap-8">
                     <div className={`reveal ${isVisible ? 'visible' : ''}`}>
-                        <DetailCard title="Format" description="Round-Robin + Grand Final to Crown the champion. All games will be held in The Embassy.">
+                        <DetailCard title="Format" description="10 teams in 2 groups. A group stage leads to a final day of placement games to crown the champion at The Embassy.">
                             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
                         </DetailCard>
                     </div>
